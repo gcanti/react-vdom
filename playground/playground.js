a66e461f30f31db4333506a58869e61f13e5c95e
gcanti <giulio.canti@gmail.com>
2014-09-04 11:13:25 +0200

    added playground

---
+++ playground/playground.js
@@ -0,0 +1,70 @@
+$(function () {
+
+  //
+  // setup
+  //
+
+  var scripts = {
+    anchor: {
+      label: 'A simple component'
+    },
+    nested: {
+      label: 'Nested components'
+    },
+    todo: {
+      label: '`TodoItem` component of React TodoMVC'
+    }
+  };
+
+  var examples = {};
+  var defaultExample = 'anchor';
+  var select = '<select id="example" class="form-control">';
+  Object.keys(scripts).forEach(function (id) {
+    examples[id] = $('#' + id).text();
+    select += '<option ';
+    if (id === defaultExample) {
+      select += ' selected="true" ';
+    }
+    select += 'value=' + id + '>' + scripts[id].label + '</option>';
+  });
+  select += '</select>'
+  $('#examples').html(select);
+
+  //
+  // eval code
+  //
+
+  var $code = $('#code');
+  var $json = $('#json');
+  var $example = $('#example');
+  var JSX_PREAMBLE = '/** @jsx React.DOM */\n';
+  function evalCode(code) {
+    try {
+      var js = JSXTransformer.transform(JSX_PREAMBLE + code).code;
+      return eval(js);
+    } catch (e) {
+      return e;
+    }
+  }
+
+  function run() {
+    var code = $code.val();
+    var x = evalCode(code);
+    if (x instanceof Error) {
+      //throw x;
+      $json.html('<div class="alert alert-danger">' + x.message + '</div>');
+    } else {
+      $json.html('<pre>' + JSON.stringify(x, null, 2) + '</pre>');
+    }
+  }
+
+  $code.val(examples[defaultExample]);
+  $('#run').click(run);
+  $example.on('change', function () {
+    var id = $(this).val();
+    $code.val(examples[id]);
+    run();
+  });
+  run();
+
+});
