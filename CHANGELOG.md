9cec5fc572fa19ddcbb4ca37c77faa1ba5b77052
gcanti <giulio.canti@gmail.com>
2015-04-29 17:06:24 +0200

    v0.2.4
    - **New Feature**
      + Support component extending React.Component #4

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,9 +1,28 @@
-v0.2.3
+# Changelog
 
-- update to tcomb v1.0, fix #2
-- upgrade to React v0.13, fix #3
+> **Tags:**
+> - [New Feature]
+> - [Bug Fix]
+> - [Breaking Change]
+> - [Documentation]
+> - [Internal]
+> - [Polish]
 
-v0.2.2
+**Note**: Gaps between patch versions are faulty/broken releases.
 
-- update to tcomb v0.4
-- add keyword react-component for discoverability
+## v0.2.4
+
+- **New Feature**
+  + Support component extending React.Component #4
+
+## v0.2.3
+
+- **Internal**
+  + update to tcomb v1.0, fix #2
+  + upgrade to React v0.13, fix #3
+
+## v0.2.2
+
+- **Internal**
+  - update to tcomb v0.4
+  - add keyword react-component for discoverability
--- test/test.js
+++ test/test.js
@@ -12,37 +12,80 @@ var eq = assert.deepEqual;
 
 describe('vdom', function () {
 
-  it('should return a assertable DOM', function () {
-    var Anchor = React.createFactory(React.createClass({displayName: 'Anchor',
-      render: function () {
+  describe('createClass', function () {
+
+    it('should return a assertable DOM', function () {
+      var Anchor = React.createFactory(React.createClass({displayName: 'Anchor',
+        render: function () {
+          return (
+            React.DOM.a({href: this.props.href}, this.props.children)
+          );
+        }
+      }));
+      var component = Anchor({href: '#section'}, 'title');
+      eq({
+        tag: 'a',
+        attrs: {href: '#section'},
+        children: 'title'
+      }, vdom(component));
+    });
+
+    it('should handle state argument', function () {
+      var Anchor = React.createFactory(React.createClass({displayName: 'Anchor',
+        render: function () {
+          return (
+            React.DOM.a({href: this.state.href}, this.props.children)
+          );
+        }
+      }));
+      var component = Anchor(null, 'title');
+      var state = {href: '#section'};
+      eq({
+        tag: 'a',
+        attrs: {href: '#section'},
+        children: 'title'
+      }, vdom(component, state));
+    });
+
+  });
+
+  describe('React.Component', function () {
+
+    it('should return a assertable DOM', function () {
+      function Anchor () {
+        React.Component.apply(this, arguments);
+      }
+      Anchor.prototype.render = function () {
         return (
           React.DOM.a({href: this.props.href}, this.props.children)
         );
-      }
-    }));
-    var component = Anchor({href: '#section'}, 'title');
-    eq({
-      tag: 'a',
-      attrs: {href: '#section'},
-      children: 'title'
-    }, vdom(component));
-  });
+      };
+      var component = new Anchor({href: '#section', children: 'title'});
+      eq({
+        tag: 'a',
+        attrs: {href: '#section'},
+        children: 'title'
+      }, vdom(component));
+    });
 
-  it('should handle state argument', function () {
-    var Anchor = React.createFactory(React.createClass({displayName: 'Anchor',
-      render: function () {
+    it('should handle state argument', function () {
+      function Anchor () {
+        React.Component.apply(this, arguments);
+      }
+      Anchor.prototype.render = function () {
         return (
           React.DOM.a({href: this.state.href}, this.props.children)
         );
-      }
-    }));
-    var component = Anchor(null, 'title');
-    var state = {href: '#section'};
-    eq({
-      tag: 'a',
-      attrs: {href: '#section'},
-      children: 'title'
-    }, vdom(component, state));
+      };
+      var component = new Anchor({children: 'title'});
+      var state = {href: '#section'};
+      eq({
+        tag: 'a',
+        attrs: {href: '#section'},
+        children: 'title'
+      }, vdom(component, state));
+    });
+
   });
 
 });
